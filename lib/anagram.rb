ff7755846c2692990c0670d2890dd5e158431bb1
may arden <mayard3n@gmail.com>
2020-10-31 15:58:28 -0700

    commit tree 9b8cccfa79259c83b97948eb29c334b37abc1555

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+
+# objectives Build a class with an initialize method and use an attr_accessor
+require 'pry'
+
+class Anagram
+
+    attr_accessor :name
+
+    def initialize(word)
+        @word = word 
+    end
+
+    def match(array)
+        anagram_array = []
+
+        array.each do |element|
+            if element.split("").sort == @word.split("").sort
+                anagram_array << element
+            end 
+        end 
+        return anagram_array 
+    end
+
+end
+
+
+
+
